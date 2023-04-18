18b084def60d0b7c7bd1547b6692979ce5d0a870
solomondax <114422582+solomondax@users.noreply.github.com>
2023-04-18 23:03:44 +0530

    yeah..

--- src/App.js
+++ src/App.js
@@ -1,28 +1,7 @@
-//import LetteCounter from "./LetterCounter"
-//import LetteCounter from "./LetterCounter"
-//import RandomNumbers from "./RandomnNumbers"
-import React,{ useEffect,useState} from 'react'
+import Capitals from './components/Capitals'
 
-const App = () => {
-  const [data, setData] = useState(0)
-  const [para,setPara]= useState(true)
-  useEffect(() => {
-    console.log("iam useEffect")
-  })
-  return (
-    <div>
-      <center>
-        <h1>useEffecthello</h1>
-        <h1 onClick={()=>{setPara(!para)}}>{para?"dax":"solo"}</h1>
-        <button onClick={() =>  setData(data + 1) }>click</button>
-        {data}
-      </center>
-     
-      </div>
-  )
+import './App.css'
 
-  
-}
-   
+const App = () => <Capitals />
 
 export default App
